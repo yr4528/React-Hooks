318d83dc6faf68567f6a40c68152b728584f3fe4
yr4528 <yr4528@naver.com>
2021-01-29 22:49:26 +0900

    210129

---
+++ useFullScreen/useFullScreen.js
@@ -0,0 +1,51 @@
+export const useFullscreen = (callback) => {
+  const element = useRef();
+  const runCb = (isFull) => {
+    if (callback && typeof callback === "function") {
+      callback(isFull);
+    }
+  };
+  const triggerFull = () => {
+    if (element.current) {
+      if (element.current.requestFullscreen) {
+        element.current.requestFullscreen();
+      } else if (element.current.mozRequestFullscreen) {
+        element.current.mozRequestFullscreen();
+      } else if (element.current.webkitRequestFullscreen) {
+        element.current.webkitRequestFullscreen();
+      } else if (element.current.msRequestFullscreen) {
+        element.current.msRequestFullscreen();
+      }
+      runCb(true);
+    }
+  };
+  const exitFull = () => {
+    document.exitFullscreen();
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullscreen) {
+      document.mozCancelFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    runCb(false);
+  };
+  return { element, triggerFull, exitFull };
+};
+
+/*
+  const App = () => {
+    const { element, triggerFull, exitFull } = useFullscreen();
+    return (
+      <div className="App" style={{ height: "1000vh" }}>
+        <div ref={element}>
+          <img src="" />
+          <button onClick={exitFull}>Exit Fullscreen</button>
+        </div>
+        <button onClick={triggerFull}>Make Fullscreen</button>
+      </div>
+    );
+  };
+  */
---
+++ useScroll/useScroll.js
@@ -0,0 +1,25 @@
+export const useScroll = () => {
+  const [state, setState] = useState({
+    x: 0,
+    y: 0,
+  });
+  const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+  return state;
+};
+
+/*
+  const App = () => {
+    const { y } = useScroll();
+    return (
+      <div className="App" style={{ height: "1000vh" }}>
+        <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h1>
+      </div>
+    );
+  };
+  */
